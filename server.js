6109b39fab548d479bb242907fbbb12a7a4402f1
Glitch (troubled-spinach) <none>
2018-03-30 18:10:11 +0000

    🎍📒 Checkpoint ./server.js:538599/94

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const request = require('request-promise');
 
 const shopBaseUrl = 'https://' + process.env.API_KEY + ':' + process.env.PASSWORD + '@' + process.env.SHOPIFY_DOMAIN;
 
+const newProductDays = 1;  
+
+
 app.get("/", (req, res) => {
   
   const shopRequestUrl = shopBaseUrl + '/admin/products.json';
